45c445116e2975a201a64569aa1e30fc7e1e1f83
S Ganga Prasath <s.gangaprasath1@gmail.com>
2023-07-19 06:32:08 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,21 +1,11 @@
 // /** @type {import('next').NextConfig} */
-// const nextConfig = {}
-
-// // module.exports = nextConfig
-
-// const nextConfig = {
-//   output: 'export',
-//   // Optional: Add a trailing slash to all paths `/about` -> `/about/`
-//   // trailingSlash: true,
-//   // Optional: Change the output directory `out` -> `dist`
-//   // distDir: 'dist',
-// }
 /**
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
   output: 'export',
   reactStrictMode: true,
+  path: '/',
  // Add basePath
   basePath: '/nextjs',
   // Optional: Add a trailing slash to all paths `/about` -> `/about/`
